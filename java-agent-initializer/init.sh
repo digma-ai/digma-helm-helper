48f41833311cd19837d35f54d20908839d37d3de
asafchen-dig <achen@digma.ai>
2024-04-03 17:31:33 +0300

    better

--- java-agent-initializer/init.sh
+++ java-agent-initializer/init.sh
@@ -2,11 +2,11 @@
 
 apk --no-cache add curl
 
-curl -L "https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v2.1.0/opentelemetry-javaagent.jar" -o opentelemetry-javaagent.jar
-curl -L "https://github.com/digma-ai/otel-java-instrumentation/releases/latest/download/digma-otel-agent-extension.jar" -o digma-otel-agent-extension.jar
+curl -L "https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v2.1.0/opentelemetry-javaagent.jar" -o otel-agent.jar
+curl -L "https://github.com/digma-ai/otel-java-instrumentation/releases/latest/download/digma-otel-agent-extension.jar" -o dig-ext.jar
 
-cp  opentelemetry-javaagent.jar /shared/java-agent/opentelemetry-javaagent.jar
-cp  digma-otel-agent-extension.jar /shared/java-agent/digma-otel-agent-extension.jar
+cp  otel-agent.jar /shared-vol/otel-agent.jar
+cp  dig-ext.jar /shared-vol/dig-ext.jar
 
 # Sleep for debugging purposes
 # sleep infinity
